92dec8a4b03603359f9158fa6e598bef9372f166
Michele Bevilacqua <bevilacqua@di.uniroma1.it>
2021-02-02 12:31:33 +0100

    update

--- scripts/ws_soc.sh
+++ scripts/ws_soc.sh
@@ -20,13 +20,13 @@ while(( $current_seed < $max_seeds ))
 do
     python run_model.py --do_train --do_lower_case \
       --data_dir ./data/waseem-sexism \
-      --bert_model distilbert-base-uncased \
+      --bert_model bert-base-uncased \
       --max_seq_length 128 \
       --train_batch_size 32 \
       --learning_rate 2e-5 \
       --num_train_epochs 20 \
       --early_stop 5 \
-      --output_dir runs/ws_es_reg_nb5_h5_is_bal_new_seed_${current_seed} \
+      --output_dir runs/waseem-sexism-soc_new_seed_${current_seed} \
        --seed ${current_seed} \
        --task_name ws \
        --reg_explanations \
@@ -36,7 +36,6 @@ do
        --reg_strength 0.1 \
        --lm_dir runs/lm_ws \
        --hiex_add_itself \
-       --neutral_words_file data/identity.csv \
-       --no_cuda
+       --neutral_words_file data/identity.csv
     let current_seed++
 done
